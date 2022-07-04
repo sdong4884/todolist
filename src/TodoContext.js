49981ec28070d34e455a1a5553a10cf5d6390965
sdong4884 <sdong4884@gmail.com>
2022-07-04 23:24:44 +0900

    todolist study with lecture

---
+++ src/TodoContext.js
@@ -0,0 +1,89 @@
+import React, { createContext, useContext, useReducer, useRef } from "react";
+
+const initialTodos = [
+  {
+    id: 1,
+    text: "프로젝트 생성하기",
+    done: true,
+  },
+  {
+    id: 2,
+    text: "컴포넌트 스타일링하기",
+    done: true,
+  },
+  {
+    id: 3,
+    text: "Context 만들기",
+    done: false,
+  },
+  {
+    id: 4,
+    text: "기능 구현하기",
+    done: false,
+  },
+];
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return state.concat(action.todo);
+    case "TOGGLE":
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, done: !todo.done } : todo
+      );
+    case "REMOVE":
+      return state.filter((todo) => todo.id !== action.id);
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+const TodoStateContext = createContext();
+const TodoDispatchContext = createContext();
+const TodoNextIdContext = createContext();
+
+export function TodoProvider({ children }) {
+  const [state, dispatch] = useReducer(todoReducer, initialTodos);
+  const nextId = useRef(5);
+
+  return (
+    <TodoStateContext.Provider value={state}>
+      <TodoDispatchContext.Provider value={dispatch}>
+        <TodoNextIdContext.Provider value={nextId}>
+          {children}
+        </TodoNextIdContext.Provider>
+      </TodoDispatchContext.Provider>
+    </TodoStateContext.Provider>
+  );
+}
+
+export function useTodoState() {
+  const context = useContext(TodoStateContext);
+  if (!context) {
+    throw new Error("Cannot find TodoProvider");
+  }
+  return context;
+}
+
+export function useTodoDispatch() {
+  const context = useContext(TodoDispatchContext);
+  if (!context) {
+    throw new Error("Cannot find TodoProvider");
+  }
+  return context;
+}
+
+/* 
+  state, dispatch를 따로 내보내기 하는 이유 ?
+  - 최적화.
+  - TodoCreate에서는 dispatch만 필요로 하게 되는데, 랜더링할 필요가 없을때에도 렌더링이 된다.
+  - 따로 내보내기 하여 TodoCreate에 dispatch만 넣어주면 state가 바뀌어도 TodoCreate는 리렌더링 되지 않는다.
+*/
+
+export function useTodoNextId() {
+  const context = useContext(TodoNextIdContext);
+  if (!context) {
+    throw new Error("Cannot find TodoProvider");
+  }
+  return context;
+}
